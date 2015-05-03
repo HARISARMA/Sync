eab0fc04d22dd6a33b1b09dad63a7e272e392170
hari1699 <harihara.sarma@RACKSPACE.COM>
2015-05-03 17:01:20 +0530

    CSUP, CSFP, BACP Sync Piece Code

---
+++ upgrade.sh
@@ -0,0 +1,12 @@
+
+pip install opencafe/ --upgrade
+
+pip install ubroast/ --upgrade
+
+pip install ubcafe/ --upgrade
+
+pip install signupcafe/ --upgrade
+
+pip install atomhoppercafe/ --upgrade 
+
+#pip install signuproast/ --upgrade
